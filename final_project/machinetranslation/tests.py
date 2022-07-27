a739d93f3f4c9941b1774448b9398492979af7b7
Behrooz <40363979+BehroozMoniri@users.noreply.github.com>
2022-07-27 16:51:40 +0100

    changed names of tests
    changed names of tests

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -3,13 +3,13 @@ import unittest
 from translator import french_to_english , english_to_french
 
 class Test(unittest.TestCase):
-    def test1(self):
-        self.assertNotEqual(french_to_english(None), "")
-    def test2(self):
-        self.assertNotEqual(english_to_french(None),"")
-    def test3(self):
+    def test_french_to_english(self):
+        self.assertNotEqual(french_to_english(None), None )
+    def test_english_to_french(self):
+        self.assertNotEqual(english_to_french(None),None)
+    def test_english_to_french_(self):
         self.assertEqual(english_to_french("Hello"),"Bonjour")
-    def test4(self):
+    def test_french_to_english_(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
 
 if __name__=="__main__":
